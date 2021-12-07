ed769b034c38ab11ae9eb02e16faa43d49e01869
Tsuku-Yomi <523896791@qq.com>
2021-12-07 11:05:44 +0800

    完成根据屏幕自动适配地图大小

--- Assets/PosSetter/MapPosBasement.cs
+++ Assets/PosSetter/MapPosBasement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MapPosBasement : MonoBehaviour
 {
@@ -8,9 +9,26 @@ public class MapPosBasement : MonoBehaviour
     public float heightInScreen = 0.2f;
     [Tooltip("·¶Î§´¦ÓÚ0~1")]
     public float widthInScreen = 0;
+    [SerializeField]
+    private float screenToWorldScale;
     public int mapHeight = 3;
     public int mapWidth = 3;
     private void OnEnable() {
-        
+        ResetMapPos();
+    }
+
+    private void ResetMapPos() {
+        screenToWorldScale = Mathf.Min(
+            Camera.main.ScreenToWorldPoint(
+                new Vector3(0, Screen.width)).y 
+            - Camera.main.ScreenToWorldPoint(
+                new Vector3(0, 0)).y, 
+            Camera.main.ScreenToWorldPoint(
+                new Vector3(Screen.height,0)).x 
+            - Camera.main.ScreenToWorldPoint(
+                new Vector3(0, 0)).x);
+        transform.localScale = new Vector3(screenToWorldScale / mapHeight, screenToWorldScale / mapWidth, 1);
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * widthInScreen, Screen.height * heightInScreen));
+        transform.position = new Vector3(transform.position.x+transform.localScale.x*0.5f, transform.position.y+transform.localScale.y*0.5f, 0);
     }
 }
